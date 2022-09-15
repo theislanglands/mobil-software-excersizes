23c7005c0a7f150d5a6c4c4fbe4651d90b7c5b67
Theis Langlands <thlan20@student.sdu.dk>
2022-09-15 14:48:14 +0200

    add fetch detail data

--- Moviedetails.js
+++ Moviedetails.js
@@ -1,17 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function MovieDetails({ route }) {
 
-    const [detailData, setDetailsData] = useState({});
-    
-    // getting selected movieId
-    const {movieId} = route.params;
+
+ // State holding movie detailData.
+ const [detailData, setdetailData] = useState({});
+
+ // Get movieID from navigate function
+ const { movieId } = route.params;
+
+ // When this component is mounted then fire the getDetails function
+ useEffect(() => {
+   getDetails();
+ }, []);
+
+ // Function to fetch details about the movie
+ function getDetails() {
+   fetch(
+     `https://api.themoviedb.org/3/movie/550?api_key=c91afe4096b39d47ada91b74ed374b93`
+   )
+     .then((response) => response.json())
+     .then((data) => setdetailData(data));
+ }
+
+ console.log(detailData);
+
+
 
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-          <Text>New Details Screen with movie id {movieId}</Text>
+            <Text>New Details Screen with movie id {movieId}</Text>
         </View>
-      );
+    );
 }
