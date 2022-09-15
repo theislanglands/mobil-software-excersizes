2ed79fb3ff94cc8e3a5b434dd98acfe86be04192
Theis Langlands <thlan20@student.sdu.dk>
2022-09-15 15:26:41 +0200

    poster showing

--- Moviedetails.js
+++ Moviedetails.js
@@ -1,6 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import { useEffect, useState } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, Image, ScrollView } from 'react-native';
+
+
 
 export default function MovieDetails({ route }) {
 
@@ -10,6 +12,7 @@ export default function MovieDetails({ route }) {
 
  // Get movieID from navigate function
  const { movieId } = route.params;
+ console.log(movieId)
 
  // When this component is mounted then fire the getDetails function
  useEffect(() => {
@@ -19,7 +22,7 @@ export default function MovieDetails({ route }) {
  // Function to fetch details about the movie
  function getDetails() {
    fetch(
-     `https://api.themoviedb.org/3/movie/550?api_key=c91afe4096b39d47ada91b74ed374b93`
+     `https://api.themoviedb.org/3/movie/${movieId}?api_key=c91afe4096b39d47ada91b74ed374b93`
    )
      .then((response) => response.json())
      .then((data) => setdetailData(data));
@@ -31,7 +34,28 @@ export default function MovieDetails({ route }) {
 
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>New Details Screen with movie id {movieId}</Text>
+            <Text>{detailData.title}</Text>
+            <Image
+                style={styles.image}
+                source={{uri: `https://image.tmdb.org/t/p/original${detailData.poster_path}`,
+            }}>
+            </Image>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "#fff",
+    },
+    title: {
+      fontSize: 28,
+      marginBottom: 10,
+    },
+    image: {
+      width: "100%",
+      height: "300px",
+    },
+  });
+  
